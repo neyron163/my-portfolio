f71db556f7005d748f4104a7dab0395850006627
neyron <andrey163783@gmail.com>
2019-07-26 11:56:43 +0400

    init

---
+++ src/components/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Home} from './home/home';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import s from "./home.module.scss";
+
+export const Home = () => (
+  <div className={s.home}>
+    <div className={s.progress}>
+      <div>My portfolio in progress</div>
+      😄
+    </div>
+      <div className={s.description}>You may already look at something interesting</div>
+      <div className={s.info}>
+      <a href="https://github.com/neyron163" className={s.github}>My github</a>
+      <div>
+        <a href="/task-manager" className={s.here}>
+          Task manager
+        </a>
+      </div>
+    </div>
+  </div>
+);
