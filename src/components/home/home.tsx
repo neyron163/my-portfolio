360e91929b27c1dedd276ee36bf1cd88a679b031
neyron <andrey163783@gmail.com>
2019-07-27 09:15:44 +0400

    some fixes

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -8,14 +8,17 @@ export const Home = () => (
       <div>My portfolio in progress</div>
       😄
     </div>
-      <div className={s.description}>You may already look at something interesting</div>
-      <div className={s.info}>
-      <a href="https://github.com/neyron163" className={s.github}>My github</a>
-      <div>
-        <a href="/task-manager" className={s.here}>
-          Task manager
-        </a>
-      </div>
+    <div className={s.description}>
+      You may already look at something interesting
+    </div>
+    <div className={s.info}>
+      <a href="https://github.com/neyron163" className={s.github}>
+        My github
+      </a>
+      <a href="/manager" className={s.here}>
+        Task manager
+      </a>
+      <a href="/aviasales" className={s.aviasales}>Aviasales</a>
     </div>
   </div>
 );
