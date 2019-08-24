4ae475354edd5e65dabbc82cf1eef24fd2977b76
neyron <andrey163783@gmail.com>
2019-08-24 21:50:37 +0400

    description

--- src/data.ts
+++ src/data.ts
@@ -15,7 +15,7 @@ export const Products = [
     link: "http://delanosolutions.com/",
     text: 'Move to Website',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Delano my second project and favorite, its design looks strong and powerful. When I started to do it I was happy to see so amazing design, I gave my soul for markup for this project and circle slider that there I spent 1/4 time to implement it.",
   },
   {
     image: deepBlueAir,
@@ -23,7 +23,7 @@ export const Products = [
     link: "http://deepblueairinc.net/",
     text: 'Move to Website',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "The first project that I got with cool animation on the main page, markup and javascript I did one, the client likes fishes and I tried to as could for him.",
   },
   {
     image: tovi,
@@ -39,7 +39,7 @@ export const Products = [
     link: "http://neyron.info/aviasales/",
     text: 'Move to Website',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "My friend found an old test task from aviasales, it was simple and basically checking the knowledge about arrays. They suggest take data from the server, I did not do connect to the server and mostly worked with arrays and some markup.",
   },
   {
     image: taskManager,
@@ -47,6 +47,6 @@ export const Products = [
     link: "http://neyron.info/taskManager/",
     text: 'Move to Website',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "The test task from megapolis, these guys suggest passing this step before you send your CV them, in the beginning, I did  markup and took data from their server and after that, I added my own server with express.js, now I take data from own server with own base data, you can check it on http://neyron.info/api/list",
   },
 ];
